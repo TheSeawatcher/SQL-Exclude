b59bd54a1f368d662c743f64e2224b02a630394e
Dean Taylor <dean@jtresponse.co.uk>
2016-04-09 23:09:46 +0100

    Create SQL-Exclude-unkowen-column.php

---
+++ SQL-Exclude-unkowen-column.php
@@ -0,0 +1,15 @@
+	function sql_exclude($table_name){
+		global $wpdb;
+		//Get all column where column comment is not private
+		$sql = $wpdb->prepare("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME= %s AND COLUMN_COMMENT != 'Private'",$this->prefix.$table_name);
+		$columns_object = $wpdb->get_results($sql);
+		$columns = "";
+		
+	
+		foreach($columns_object as $key=>$column){
+		 	$columns .= $column->COLUMN_NAME.",";
+		 }
+		 $columns = rtrim($columns,",");
+		 $sql = $wpdb->prepare("SELECT %s FROM %s",$columns,$this->prefix.$table_name);
+	     return($wpdb->get_results($sql));	
+	}
